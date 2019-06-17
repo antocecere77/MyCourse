6eb57620df808a25d04311a7558483ba7835676c
Antonio Cecere <antocecere77@gmail.com>
2019-06-17 07:06:18 +0200

    Aggiunta protezione da sql injection

--- src/MyCourse/Models/Services/Application/AdoNetCouseService.cs
+++ src/MyCourse/Models/Services/Application/AdoNetCouseService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using MyCourse.Models.Services.Infrastructure;
@@ -17,7 +18,7 @@ namespace MyCourse.Models.Services.Application
 
         public List<CourseViewModel> GetCourses()
         {
-            string query = "SELECT Id, Title, Author, ImagePath, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
+            FormattableString query = $"SELECT Id, Title, Author, ImagePath, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
             DataSet dataSet = db.Query(query);
             var dataTable = dataSet.Tables[0];
             var courseList = new List<CourseViewModel>();
@@ -30,7 +31,28 @@ namespace MyCourse.Models.Services.Application
 
         public CourseDetailViewModel GetCourse(int id)
         {
-            throw new System.NotImplementedException();
+            FormattableString query = $@"SELECT Id, Title, Description, ImagePath, Author, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses WHERE Id= {id}; 
+            SELECT Id, Title, Description, Duration FROM Lessons WHERE CourseId= {id}";
+
+            DataSet dataSet = db.Query(query);
+
+            //Coursek
+            var courseTable = dataSet.Tables[0];
+            if(courseTable.Rows.Count!=1) {
+                throw new InvalidOperationException($"Did nou return exactly 1 row for Course {id}");
+            }
+
+            var courseRow = courseTable.Rows[0];
+            var courseDetailViewModel = CourseDetailViewModel.FromDataRow(courseRow);
+
+            //Course lessons
+            var lessonDataTable = dataSet.Tables[1];
+            foreach(DataRow lessonRow in lessonDataTable.Rows) {
+                LessonViewModel lessonViewModel = LessonViewModel.FromDataRow(lessonRow);
+                courseDetailViewModel.Lessons.Add(lessonViewModel);
+            }
+
+            return courseDetailViewModel;
         }
 
     }
--- src/MyCourse/Models/Services/Infrastructure/SqlliteDatabaseAccessor.cs
+++ src/MyCourse/Models/Services/Infrastructure/SqlliteDatabaseAccessor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
 using Microsoft.Data.Sqlite;
 
@@ -5,21 +7,37 @@ namespace MyCourse.Models.Services.Infrastructure
 {
     public class SqlliteDatabaseAccessor : IDatabaseAccessor
     {
-        public DataSet Query(string query)
+        public DataSet Query(FormattableString formattableQuery)
         {
+
+            var queryArguments = formattableQuery.GetArguments();
+            var sqliteParameters = new List<SqliteParameter>();
+            for(var i = 0; i <queryArguments.Length; i++) {
+                var parameter = new SqliteParameter(i.ToString(), queryArguments[i]);
+                sqliteParameters.Add(parameter);
+                queryArguments[i] = "@" + i;
+            }
+
+            string query = formattableQuery.ToString();
+
             using(var conn = new SqliteConnection("Data Source=Data/MyCourse.db"))
             {
                 conn.Open();
                 using(var cmd = new SqliteCommand(query, conn)) 
                 {
+                    cmd.Parameters.AddRange(sqliteParameters);
                     using(var reader = cmd.ExecuteReader())
                     {
                         var dataSet = new DataSet();
                         dataSet.EnforceConstraints = false;
-                        var dataTable = new DataTable();
-                        dataSet.Tables.Add(dataTable);
 
-                        dataTable.Load(reader);
+                        do
+                        {
+                            var dataTable = new DataTable();
+                            dataSet.Tables.Add(dataTable);
+                            dataTable.Load(reader);
+                        } while(!reader.IsClosed);
+                      
                         return dataSet;
                     }
                     
