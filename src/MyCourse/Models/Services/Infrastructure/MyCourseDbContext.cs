2947d4b5730cc96b58637cc8f5061b3fcd8097db
Antonio Cecere <antocecere77@gmail.com>
2019-06-22 15:47:36 +0200

    Aggiunte classi di gestione db con tecnica database first

---
+++ src/MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MyCourse.Models.Entities;
+
+namespace MyCourse.Models.Services.Infrastructure
+{
+    public partial class MyCourseDbContext : DbContext
+    {
+        public MyCourseDbContext()
+        {
+        }
+
+        public MyCourseDbContext(DbContextOptions<MyCourseDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Lesson> Lessons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=Data/MyCourse.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.ToTable("Courses");  //Superfluo se la tabella si chiama come la proprietà
+                entity.HasKey(course => course.Id); //Superfluo se la proprietà si chiama Id oppure CoursesId
+                //entity.HasKey(course => new { course.Id, course.Author }); //Per chiavi primarie composite (è importante rispettare l'ordine dei campi)
+            
+                #region Mapping generato automaticamente dal tool reverse engineering
+                /*
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Author)
+                    .IsRequired()
+                    .HasColumnType("TEXT (100)");
+
+                entity.Property(e => e.CurrentPriceAmount)
+                    .IsRequired()
+                    .HasColumnName("CurrentPrice_Amount")
+                    .HasColumnType("NUMERIC")
+                    .HasDefaultValueSql("0");
+
+                entity.Property(e => e.CurrentPriceCurrency)
+                    .IsRequired()
+                    .HasColumnName("CurrentPrice_Currency")
+                    .HasColumnType("TEXT (3)")
+                    .HasDefaultValueSql("'EUR'");
+
+                entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
+
+                entity.Property(e => e.Email).HasColumnType("TEXT (100)");
+
+                entity.Property(e => e.FullPriceAmount)
+                    .IsRequired()
+                    .HasColumnName("FullPrice_Amount")
+                    .HasColumnType("NUMERIC")
+                    .HasDefaultValueSql("0");
+
+                entity.Property(e => e.FullPriceCurrency)
+                    .IsRequired()
+                    .HasColumnName("FullPrice_Currency")
+                    .HasColumnType("TEXT (3)")
+                    .HasDefaultValueSql("'EUR'");
+
+                entity.Property(e => e.ImagePath).HasColumnType("TEXT (100)");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnType("TEXT (100)");
+                */
+                #endregion
+            });
+
+            modelBuilder.Entity<Lesson>(entity =>
+            {
+                #region Mapping generato automaticamente dal tool reverse engineering
+                /*
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
+
+                entity.Property(e => e.Duration)
+                    .IsRequired()
+                    .HasColumnType("TEXT (8)")
+                    .HasDefaultValueSql("'00:00:00'");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnType("TEXT (100)");
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Lessons)
+                    .HasForeignKey(d => d.CourseId);
+                */
+                #endregion
+            });
+        }
+    }
+}
