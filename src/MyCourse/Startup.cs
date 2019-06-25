4cf850b8a1c23862f9d7183f4affd353302987e4
Antonio Cecere <antocecere77@gmail.com>
2019-06-25 07:43:42 +0200

    Aggiunta gestione connection string con Microsoft.Extensions.Configuration

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyCourse.Models.Services.Application;
 using MyCourse.Models.Services.Infrastructure;
@@ -17,6 +18,13 @@ namespace MyCourse
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,8 +38,8 @@ namespace MyCourse
             //services.AddScoped<MyCourseDbContext>(); //Equivalente alla riga sotto
             //services.AddDbContext<MyCourseDbContext>();
             services.AddDbContextPool<MyCourseDbContext>(optionsBuilder => {
-                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlite("Data Source=Data/MyCourse.db");
+                string connectionString = Configuration.GetSection("ConnectionsStrings").GetValue<string>("Default");
+                optionsBuilder.UseSqlite(connectionString);
             });
         }
 
