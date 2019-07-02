17dad99256ddba0250d2d8ca9616d0eeedd08373
Antonio Cecere <antocecere77@gmail.com>
2019-07-02 07:42:01 +0200

    Aggiunta gestione contenuto pagina su cache (HomeController)

--- src/MyCourse/Controllers/HomeController.cs
+++ src/MyCourse/Controllers/HomeController.cs
@@ -2,8 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyCourse.Controllers
 {
+    //L'attributo avrebbe effetto su tutte le action del controller
+    //[ResponseCache(CacheProfileName="Home")]
     public class HomeController : Controller
     {
+        //[ResponseCache(Duration=60, Location=ResponseCacheLocation.Client)]
+        [ResponseCache(CacheProfileName="Home")]
         public IActionResult Index() {            
             return View();            
         }
--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -31,7 +31,16 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+                {
+                    var homeProfile = new CacheProfile();
+                    //homeProfile.Duration = Configuration.GetValue<int>("ResponseCache:Home:Duration");
+                    //homeProfile.Location = Configuration.GetValue<ResponseCacheLocation>("ResponseCache:Home:Location");
+                    //homeProfile.VaryByQueryKeys = new string[] { "page" };
+                    Configuration.Bind("ResponseCache:Home", homeProfile);
+                    options.CacheProfiles.Add("Home", homeProfile);
+                }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                
             services.AddTransient<ICourseService, AdoNetCouseService>();
             //services.AddTransient<ICourseService, EFCoreCoursesService>();
             services.AddTransient<IDatabaseAccessor, SqlliteDatabaseAccessor>();
