6713cd609b82de4b2faf1323234f17e5f1471772
Antonio Cecere <antocecere77@gmail.com>
2019-07-05 07:18:26 +0200

    Aggiunta gestione log con Serilog

--- src/MyCourse/Program.cs
+++ src/MyCourse/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace MyCourse
 {
@@ -19,6 +20,10 @@ namespace MyCourse
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog((webHostBuilderContext, loggerConfiguration) => 
+                {
+                    loggerConfiguration.ReadFrom.Configuration(webHostBuilderContext.Configuration);
+                });
     }
 }
